9dd668892b684f0eb48c04bbddc01c0d5bae498d
Winfred Huang <huang.winfred21@gmail.com>
2022-06-06 11:18:19 -0700

    Add styling touches

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends React.Component {
     super(props);
 
     this.state = {
-      markdownText: "# React Markdown Previewer\n## What you see on the left is will appear on the right.\nIn this sample previewer, I want to show you how to print `Hello world`.\n\nBefore we can get started, here are some pre-requisities.\n\n1. You know how to read code\n2. You know what a function is\n3. You know how simple this is\n\nWithout further ado, here is the **code**.\n```function myFunction() {\nconsole.log('Hello World'); }```\n\nThat was simple right? Hope you enjoyed this little exercise!\n\nThis project is one of many projects on [freeCodeCamp](https://www.freecodecamp.org) website!\n\n![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)"
+      markdownText: "# React Markdown Previewer\n## What you see on the left is will appear on the right.\nIn this sample previewer, I want to show you how to print `Hello world`.\n\nBefore we can get started, here are some prerequisites.\n\n1. You know how to read code\n2. You know what a function is\n3. You know how simple this is\n\nWithout further ado, here is the **code**.\n```\nfunction myFunction() {\n\tconsole.log('Hello World'); \n}\n```\n\nThat was simple right? Hope you enjoyed this little exercise!\n\nThis project is one of many projects on [freeCodeCamp](https://www.freecodecamp.org) website!\n\n![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)"
     }
 
     this.handleChange = this.handleChange.bind(this);
@@ -20,16 +20,14 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <div>
-          <div>
-            <h1>Markdown Editor</h1>
-            <textarea id='editor' onChange={this.handleChange} value={this.state.markdownText}></textarea>
-          </div>
-          <div>
-            <h1>Markdown Previewer</h1>
-            <div id='preview'>
-              <Markdown>{this.state.markdownText}</Markdown>
-            </div>
+        <div className='markdown-container'>
+          <h1>Markdown Editor</h1>
+          <textarea id='editor' onChange={this.handleChange} value={this.state.markdownText}></textarea>
+        </div>
+        <div className='markdown-container'>
+          <h1>Markdown Previewer</h1>
+          <div id='preview'>
+            <Markdown>{this.state.markdownText}</Markdown>
           </div>
         </div>
       </div>
