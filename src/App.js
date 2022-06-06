6b8851b2f0cf3a3f5a674afd41241f0fb489562b
Winfred Huang <huang.winfred21@gmail.com>
2022-06-06 11:41:30 -0700

    Edit changes to default html text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends React.Component {
     super(props);
 
     this.state = {
-      markdownText: "# React Markdown Previewer\n## What you see on the left is will appear on the right.\nIn this sample previewer, I want to show you how to print `Hello world`.\n\nBefore we can get started, here are some prerequisites.\n\n1. You know how to read code\n2. You know what a function is\n3. You know how simple this is\n\nWithout further ado, here is the **code**.\n```\nfunction myFunction() {\n\tconsole.log('Hello World'); \n}\n```\n\nThat was simple right? Hope you enjoyed this little exercise!\n\nThis project is one of many projects on [freeCodeCamp](https://www.freecodecamp.org) website!\n\n![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)"
+      markdownText: "# React Markdown Previewer\n## What you see here will appear below.\nIn this sample previewer, I want to show you how to print `Hello world`.\n\n>Feel free to change this editor to play around!\n\nBefore we can get started, here are some prerequisites.\n\n1. You know how to read code\n2. You know what a function is\n3. You know how simple this is\n\nWithout further ado, here is the **code**.\n```\nfunction myFunction() {\n\tconsole.log('Hello World'); \n}\n```\n\nThat was simple right? Hope you enjoyed this little exercise!\n\nThis project is one of many projects on [freeCodeCamp](https://www.freecodecamp.org) website!\n\n![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)"
     }
 
     this.handleChange = this.handleChange.bind(this);
